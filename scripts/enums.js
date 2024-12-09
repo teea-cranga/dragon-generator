616f6aae8efa6c3a019d725034022b1531a04764
Teea Cranga <crangaana22@stud.ase.ro>
2024-12-09 17:56:22 +0200

    Made the main page, along with a "prototype" design.

---
+++ scripts/enums.js
@@ -0,0 +1,16 @@
+const dragonBody={
+    Wyrm: 1,
+    Drake: 2,
+    Wyvern: 3,
+    TwoHeaded: 4,
+    Pygmy: 5
+};
+
+const dragonElement={
+    light: 0,
+    dark: 1,
+    fire: 2,
+    water: 3,
+    earth: 4,
+    wind: 5
+}
