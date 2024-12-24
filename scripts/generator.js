cbabd6ceeceb40543a925db478c1317013304517
Teea Cranga <crangaana22@stud.ase.ro>
2024-12-24 09:41:12 +0200

    Managed to do the generate function for all fields, added a "Generate all" button (not working yet), and have to center the divs again at one point.

--- scripts/generator.js
+++ scripts/generator.js
@@ -2,7 +2,7 @@ window.onload=function(){
     //declare the buttons
     let butType = document.getElementById('butType');
     let butElem = document.getElementById('butElem');
-    let butWing = document.getElementById('butWings');
+    let butWing = document.getElementById('butWing');
     let butPrim = document.getElementById('butPrim');
     let butSec = document.getElementById('butSec');
     let butTert = document.getElementById('butTert');
@@ -10,12 +10,34 @@ window.onload=function(){
     //declare the inputs
     let statBody = document.getElementById('statBody');
     let statElem = document.getElementById('statElem');
+    let statWing = document.getElementById('statWing');
+    let statCol1 = document.getElementById('statCol1');
+    let statCol2 = document.getElementById('statCol2');
+    let statCol3 = document.getElementById('statCol3');
 
+    //generating values
     butType.addEventListener('click', ()=>{
-        generateBody();
+        generate(butType.id);
     });
 
     butElem.addEventListener('click', ()=>{
-        generateElement();
+        generate(butElem.id);
     });
+
+    butWing.addEventListener('click', ()=>{
+        generate(butWing.id);
+    });
+
+    butPrim.addEventListener('click', ()=>{
+        generate(butPrim.id);
+    });
+
+    butSec.addEventListener('click', ()=>{
+        generate(butSec.id);
+    });
+
+    butTert.addEventListener('click', ()=>{
+        generate(butTert.id);
+    });
+
 }
--- scripts/utils.js
+++ scripts/utils.js
@@ -1,17 +1,53 @@
-const dragonBody = ["Wyrm", "Drake", "Wyvern", "Two Headed", "Pygmy"];
+const dragonBody = ["Wyrm", "Drake", "Wyvern", "Two Headed", "Pygmy", "Leviathan"];
 
 const dragonElement = ["No power", "Light", "Darkness", "Fire", "Water", "Earth", "Wind", "Electricity", "Time", "Death", "Life"];
 
 //utils
 
 function generateBody() {
-    statBody.value = dragonBody[getRandomNumber(4)];
+    statBody.value = dragonBody[getRandomNumber(5)];
 }
 
 function generateElement() {
     statElem.value = dragonElement[getRandomNumber(10)];
 }
 
+function generate(button) {
+    switch (button) {
+        case "butType":
+            statBody.value = dragonBody[getRandomNumber(5)];
+            break;
+        case "butElem":
+            statElem.value = dragonElement[getRandomNumber(10)];
+            break;
+        case "butWing":
+            statWing.value = getRandomNumber(2) == 1 ? "Has wings" : "Doesn't have wings";
+            break;
+        case "butPrim":
+            let myPrimColor = "#" + generateColor();
+            statCol1.style.backgroundColor = myPrimColor;
+            statCol1.value = myPrimColor;
+
+            break;
+        case "butSec":
+            let mySecColor = "#" + generateColor();
+            statCol2.style.backgroundColor = mySecColor;
+            statCol2.value = mySecColor;
+            break;
+        case "butTert":
+            let myTertColor = "#" + generateColor();
+            statCol3.style.backgroundColor = myTertColor;
+            statCol3.value = myTertColor;
+            break;
+        default:
+            break;
+    }
+}
+
 function getRandomNumber(maxNumber) {
     return Math.floor(Math.random() * maxNumber);
+}
+
+function generateColor() {
+    return Math.floor(Math.random() * 16777215).toString(16);
 }
